cc2a548eb3ab825f0de4db8f56ea63c9f5edaf06
Alexey Belozerov <alex@welldonecode.com>
2024-06-19 22:48:18 +0400

    cname generation, assets paths fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  assetPrefix: isProd ? '/abelozerov/blog/' : '',
-  basePath: isProd ? '/abelozerov/blog' : '',
   output: 'export'
 };
 
